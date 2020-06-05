3c7dc4d6bb39fd2b0044b855025501acee8d3aee
Maciej Majchrzak <maciek.majchrzak15@gmail.com>
2020-06-05 15:49:46 +0200

    Provision of context and models

---
+++ src/Charity/Charity.Mvc/Models/CharityDonation/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Charity.Mvc.Models.CharityDonation
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<CategoryDonation> CategoryDonations { get; set; }
+    }
+}
---
+++ src/Charity/Charity.Mvc/Models/CharityDonation/CategoryDonation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Charity.Mvc.Models.CharityDonation
+{
+    public class CategoryDonation
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [ForeignKey("Donation")]
+        public int DontaionId { get; set; }
+        public Donation Donation { get; set; }
+    }
+}
